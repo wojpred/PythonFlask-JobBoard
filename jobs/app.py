a49631e4876d9f164a81beda67af24d15c1e722b
= <wojciech.dynda@gmail.com>
2022-02-16 10:06:36 +0100

    commit

--- jobs/app.py
+++ jobs/app.py
@@ -1,6 +1,11 @@
-from flask import Flask, render_template
+import sqlite3
+from flask import Flask, render_template, g
 
+PATH='db/jobs.sqlite'
 app = Flask(__name__)
+
+def open_connection():
+    connection = g.__getattr__(_connection)
 
 @app.route('/')
 @app.route('/jobs')
