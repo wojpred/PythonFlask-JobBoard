9945689ff197ab9098b78921909bc6f793c3ecdd
= <wojciech.dynda@gmail.com>
2022-02-16 10:23:42 +0100

    commit

--- jobs/app.py
+++ jobs/app.py
@@ -17,7 +17,7 @@ def open_connection():
 def execute_sql(sql, values=(),commit=False,single=False):
     connection = open_connection()
     cursor = connection.execute(sql, values)
-    if commit == True:
+    if commit == True:ee
         results = connection.commit()
     else:
         result = cursor.fetchone() if single else cursor.fetchall()
